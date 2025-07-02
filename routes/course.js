17d6c5bac46d514f3a97c794ffa0ee4c72609d88
Vaibhav Shukla <vaibhav.shukla0925@gmail.com>
2025-07-02 13:52:21 +0530

    created purchase and preview ends point in course

--- routes/course.js
+++ routes/course.js
@@ -1,13 +1,28 @@
 const {Router} = require("express");
-
+const {userMiddleware} = require("../middleware/user");
+const { purchaseModel, courseModel } = require("../db")
 const courseRouter = Router();
 
-courseRouter.get("/preview", function(req, res) {
+courseRouter.get("/preview", async function(req, res) {
+    const courses = await courseModel.find({});
 
+    res.json({
+        courses
+    })
 });
 
-courseRouter.post("/purchase", function(req, res) {
+courseRouter.post("/purchase", userMiddleware, async function(req, res) {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
 
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
+    res.json({
+        message: 'You Have succesfully purchased the course.'
+    })
 });
 
 module.exports = {
